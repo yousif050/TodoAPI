241842342962f5165533235905de8787dcd69c92
Yousif Albayati <yalbayat@my.centennialcollege.ca>
2025-02-03 12:55:35 -0500

    Fix issue: added quiz2

--- TodoAPI/Controllers/TodoItemsController.cs
+++ TodoAPI/Controllers/TodoItemsController.cs
@@ -22,6 +22,7 @@ namespace TodoAPI.Controllers
             {
                 _context.TodoItems.Add(new TodoItem { Name="Lab1", IsComplete=true});
                 _context.TodoItems.Add(new TodoItem { Name="Lab2",IsComplete=false});
+                _context.TodoItems.Add(new TodoItem { Name = "Quiz2", IsComplete = false });
             }
 
             _context.SaveChanges();
